b450e15080eff13a67a90bf4724d53547d82e9d0
Jorgen29 <bacolodjorgen29gmail.com>
2025-11-13 01:07:33 +0800

    Initial commit

---
+++ laravel-project-2-HMSyS/routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\AdminController;
+
+Route::get('/', [AdminController::class, 'home']);
+
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', [AdminController::class, 'index'])->name('dashboard');
+    Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+});
+
+Route::get('/home', [AdminController::class, 'index']);
